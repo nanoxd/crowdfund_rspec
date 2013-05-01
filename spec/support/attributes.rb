45807abec7efa7e75c8ffb64f93f5fe07e0ca1ae
Fernando Paredes <nanoid.xd@gmail.com>
2013-05-01 11:13:17 -0700

    Move order for ST2 bubbling.

--- spec/support/attributes.rb
+++ spec/support/attributes.rb
@@ -2,8 +2,8 @@ def project_attributes(overrides = {})
   {
     name: "Start-Up Projectz",
     description: "A description of a start-up project",
-    target_pledge_amount: 100.00,
     pledging_ends_on: 1.day.from_now,
+    target_pledge_amount: 100.00,
     website: "http://project-ab.com"
   }.merge(overrides)
 end
